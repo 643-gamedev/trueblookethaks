00ab9f58d6bde803d24c1385d20d5329228a2a9e
Dusan Jacenko <dusan.jacenko@gmail.com>
2024-10-30 06:56:57 +0000

    Update gui.js

--- cheats/gui.js
+++ cheats/gui.js
@@ -532,7 +532,7 @@
         i.remove();
         
         function getStateNode() {
-            return Object.values((function react(r = document.querySelector("body>div")) { return Object.values(r)[1]?.children?.[0]?._owner.stateNode ? r : react(r.querySelector(":scope>div")) })())[1].children[0]._owner.stateNode;
+            
         }
         
         const Cheats = {
